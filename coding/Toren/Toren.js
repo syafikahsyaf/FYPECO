a7c26311f5f4fcbf9f09cad9cc91fc32cf169b97
syafikahsyaf <syafikahsyaf05@gmail.com>
2026-01-14 01:55:41 +0800

    Update Toren.js

--- coding/Toren/Toren.js
+++ coding/Toren/Toren.js
@@ -154,7 +154,7 @@ document.addEventListener("DOMContentLoaded", () => {
         <p style="font-size:clamp(14px,3.5vw,18px); line-height:1.4">
           👉 <b>1 Tap</b> : Tukar Paparan AR <br>
           👉 <b>Drag</b> : Pusing model 3D<br>
-          👉  <b>Pinch</b> : Zoom Besar/Kecil<br><br>
+          👉 <b>Pinch(Cubit)/Scroll</b> : Zoom Besar/Kecil<br><br>
           Arahkan kamera ke <b>Imej Sasaran</b> untuk mula.
         </p>
         <button id="startARBtn"
